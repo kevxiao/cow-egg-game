1bdd03d40c8eecb0120731e6cd459fef9268e196
Kevin Xiao <kevinxiao2@gmail.com>
2014-11-09 18:57:11 -0500

    Fixed indentation

--- game.py
+++ game.py
@@ -4,24 +4,24 @@ from google.appengine.api import channel
 from google.appengine.ext import db
 
 import webapp2
-import os
 
 class GamePage(webapp2.RequestHandler):
 
-    def get(self):
-    	user = users.get_current_user()
-
-        self.response.headers.add_header('Access-Control-Allow-Origin', '*')
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.out.write(template.render("main.html", {}))
-        self.response.out.write('Hello, ' + user.nickname())
+	def get(self):
+		user = users.get_current_user()
+		if user:
+			self.response.headers.add_header('Access-Control-Allow-Origin', '*')
+			self.response.headers['Content-Type'] = 'text/html'
+			self.response.out.write(template.render("main.html", {}))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp2.WSGIApplication([
-    ('/', GamePage),
+	('/', GamePage),
 ], debug=True)
 
 def main():
-    run_wsgi_app(application)
+	run_wsgi_app(application)
 
 if __name__ == "__main__":
-    main()
+	main()
