bcab2230cc540ac8ca1831238d5fde834811a105
Kim Usi <kusi@uwaterloo.ca>
2014-11-13 13:43:05 -0500

    Separated main core pages into individual ones

--- scripts/binding.js
+++ scripts/binding.js
@@ -2,11 +2,24 @@
 var pages = document.getElementById('pages');
 var tabs = document.getElementById('tabs');
 var aboutpages = document.getElementById('aboutpages');
+var gamepages = document.getElementById('gamepages');
+
 tabs.addEventListener('core-select', function() {
-  pages.selected = tabs.selected;
-  if(pages.selected != 2) {
-  		aboutpages.selected=-1
-  }else{
-  		aboutpages.selected=0
+  // home page selected
+  if(tabs.selected == 0){ 
+    pages.selected = 0;
+    gamepages.selected = -1;
+    aboutpages.selected = -1;
+  } // game page selected
+  else if (tabs.selected == 1){
+    pages.selected = -1;
+    gamepages.selected = 0;
+    aboutpages.selected = -1;
+  } // about page selected
+  else if (tabs.selected == 2){
+    pages.selected = -1;
+    gamepages.selected = -1;
+    aboutpages.selected = 0;
   }
+
 });
--- scripts/next.js
+++ scripts/next.js
@@ -1,12 +1,15 @@
-
+// Switching pages on About page
+var tabs = document.getElementById('tabs');
 document.querySelector('.change').onclick = function(e) {
-  this.selected = (this.selected + 1) % this.items.length;
-  this.async(function() {
-    if (this.selectedIndex == 0) {
-      this.selectedItem.classList.remove('begin');
-    } else if (this.selectedIndex == this.items.length - 1) {
-      this.items[0].classList.add('begin');
+    if (tabs.selected == 2) {
+        this.selected = (this.selected + 1) % this.items.length;
+        this.async(function() {
+            if (this.selectedIndex == 0) {
+                this.selectedItem.classList.remove('begin');
+            } else if (this.selectedIndex == this.items.length - 1) {
+                this.items[0].classList.add('begin');
+            }
+        });
     }
-  });
 };
 
