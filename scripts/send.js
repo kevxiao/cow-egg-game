6b7fec262dc82c215140e09f99ab925a9644f297
Kim Usi <kusi@uwaterloo.ca>
2014-11-22 18:40:28 -0500

    added dropdown menu for hexagons

--- scripts/send.js
+++ scripts/send.js
@@ -7,7 +7,7 @@ function mapClickListener(){
             var col = this.getAttribute('col');
             console.log(col, ', ', row);
             var coord = { 'row':row, 'column':col };
-            $.ajax({
+            $.ajax({ // send col and row
                 url: '/map',
                 type: 'POST',
                 data: coord,
@@ -16,6 +16,8 @@ function mapClickListener(){
                     console.log("asdf");
                 }
             });
+            // show paper-dropdown for hex
+            hexDropClickListener();
             return false;
         });
     });
@@ -32,6 +34,43 @@ function startListener(){
                 dataType: 'json',
                 success: function(data){
                     console.log("starting game");
+                }
+            });
+            return false;
+        });
+    });
+}
+
+// creates a drop-down menu after the user clicks on a hex
+function hexDropClickListener(){ 
+    var menu, inner, i, numItems, menuItems;
+    if ( {{unit}} ){
+        menu = document.createElement('paper-dropdown');
+        form = document.createElement('form');
+        inner = document.createElement('div');
+        form.id = "dropdown";
+        form.action = "/action";
+        numItems = {{n_items}};
+        menuItems = {{menu_items}};
+        for (i = 0; i < numItems; i=i+1){ // create multiple menu items to show
+            var item = document.createElement('paper-item');
+            menu.appendChild(item);
+            item.className = 'item';
+            item.innerHTML = menuItems[i];
+        }
+    }
+    // when an item in the menu is clicked, we need to send that data to backend
+    $(document).ready(function(){
+        $('.item').click(function() { 
+            var itemClicked = this.innerHTML;
+            var data = {'item': itemClicked};
+            $.ajax({
+                url = '/action';
+                type: 'POST',
+                data: begin,
+                dataType: 'json',
+                success: function(data){
+                    console.log("giving action");
                 }
             });
             return false;
