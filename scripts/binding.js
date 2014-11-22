96168ce3b6a5e60d1f1cc2d8e72488dae17be28d
Kevin Xiao <kevinxiao2@gmail.com>
2014-11-21 19:12:23 -0500

    Fixed page binding and display

--- scripts/binding.js
+++ scripts/binding.js
@@ -8,18 +8,21 @@ tabs.addEventListener('core-select', function() {
   // home page selected
   if(tabs.selected == 0){ 
     pages.selected = 0;
-    gamepages.selected = -1;
-    aboutpages.selected = -1;
+    pages.style.display = "";
+    gamepages.style.display = "none";
+    aboutpages.style.display = "none";
   } // game page selected
   else if (tabs.selected == 1){
-    pages.selected = -1;
+    pages.style.display = "none";
     gamepages.selected = 0;
-    aboutpages.selected = -1;
+    gamepages.style.display = "";
+    aboutpages.style.display = "none";
   } // about page selected
   else if (tabs.selected == 2){
-    pages.selected = -1;
-    gamepages.selected = -1;
+    pages.style.display = "none";
+    gamepages.style.display = "none";
     aboutpages.selected = 0;
+    aboutpages.style.display = "";
   }
 
 });
