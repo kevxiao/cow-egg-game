c7e7ba001cb163dfe36f9bebacf92caac832fbef
Kim Usi <kusi@uwaterloo.ca>
2014-11-24 17:57:58 -0500

    commented stuff for demo

--- scripts/binding.js
+++ scripts/binding.js
@@ -4,33 +4,7 @@ var tabs = document.getElementById('tabs');
 var aboutpages = document.getElementById('aboutpages');
 var gamepages = document.getElementById('gamepages');
 var gamehome = document.getElementById('gamehome');
-<<<<<<< HEAD
-var button = document.querySelector('.start');
-tabs.addEventListener('core-select', function() {
-  // home page selected
-  if(tabs.selected == 0){ 
-    pages.selected = 0;
-    pages.style.display = "";
-    gamepages.style.display = "none";
-    aboutpages.style.display = "none";
-  } // game page selected
-  else if (tabs.selected == 1){
-    pages.style.display = "none";
-    gamepages.selected = 0;
-    gamepages.style.display = "";
-    gamehome.style.display = "";
-    aboutpages.style.display = "none";
-    button.disabled = false;
-  } // about page selected
-  else if (tabs.selected == 2){
-    pages.style.display = "none";
-    gamepages.style.display = "none";
-    aboutpages.selected = 0;
-    aboutpages.style.display = "";
-  }
 
-});
-=======
 if (tabs.addEventListener){
 	tabs.addEventListener('core-select', function() {
 	  // home page selected
@@ -78,4 +52,3 @@ if (tabs.addEventListener){
 	  }
 	});
 }
->>>>>>> 8a0a1569a121044617af0ad8311e1774f1c479a2
